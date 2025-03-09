f4b950c1a3db3f9ae9b96ed82ab725fa87060b64
SofiaaO <sofiaortizh1@gmail.com>
2025-03-09 17:49:38 -0400

    cambios again

--- myapp/admin.py
+++ myapp/admin.py
@@ -9,10 +9,3 @@ admin.site.register(CustomUser)
 admin.site.register(Ventas)
 admin.site.register(Impuesto)
 admin.site.register(Categoria)
-admin.site.register(Producto_Marca_Categoria)
-@admin.register(AuditLog)
-class AuditLogAdmin(admin.ModelAdmin):
-    list_display = ('usuario', 'accion', 'fecha', 'detalles')  # Muestra estos campos en el admin
-    list_filter = ('usuario', 'accion', 'fecha')  # Filtros por usuario, acción y fecha
-    search_fields = ('accion', 'detalles')  # Permite buscar por acción y detalles
-    ordering = ('-fecha',)  # Ordenar por fecha (descendente)
